00d6330015609c22b407ea2304fe25ba4d742225
Nihal <nihalkudukkan@gmail.com>
2025-07-10 22:13:15 +0530

    inital commit

---
+++ websocket-be/src/main/java/com/example/websocket_be/WebsocketBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.websocket_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketBeApplication.class, args);
+	}
+
+}
